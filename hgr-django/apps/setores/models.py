2ed4e3b4644e8ac732c37c10faf51f199ad5c8fc
Eduardo Henrique Freire Machado <edu.hen.fm@gmail.com>
2025-01-20 14:04:17 -0400

    feat

--- hgr-django/apps/setores/models.py
+++ hgr-django/apps/setores/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 
+
 class Setor(models.Model):
     cor = models.CharField(max_length=7, unique=True, verbose_name='Cor')
     nome = models.CharField(max_length=128, unique=True, verbose_name='Nome')
-    removido_em = models.DateTimeField(null=True, blank=True, verbose_name='Removido em')
+    removido_em = models.DateTimeField(
+        null=True, blank=True, verbose_name='Removido em')
 
     def get_participacao(self):
-        from apps.pacientes.models import Paciente
+        from apps.leitos.models import Leito
 
-        total = Paciente.objects.filter(removido_em__isnull=True).count()
-        partial = Paciente.objects.filter(removido_em__isnull=True, setor=self).count()
+        total = Leito.objects.filter(removido_em__isnull=True).count()
+        partial = Leito.objects.filter(
+            removido_em__isnull=True, setor=self).count()
 
         if total == 0:
             return 0
-        
+
         return (partial / total) * 100
 
     class Meta:
