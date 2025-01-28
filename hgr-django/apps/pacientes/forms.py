85f32e86e6886752dc8c1ade493fc9e3375698b9
Eduardo Henrique Freire Machado <edu.hen.fm@gmail.com>
2025-01-28 11:10:21 -0400

    fix

--- hgr-django/apps/pacientes/forms.py
+++ hgr-django/apps/pacientes/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.db.models import Q
 from .models import Paciente
 from apps.leitos.models import Leito
 from apps.saidas.models import Saida
@@ -12,17 +13,18 @@ from apps.tipos_de_vacuo.models import TipoDeVacuo
 from apps.tipos_de_cirurgia.models import TipoDeCirurgia
 from apps.nacionalidades_etnias.models import NacionalidadeEtnia
 from apps.generos.models import Genero
+from apps.usuarios.models import Usuario
 from django.utils.timezone import now
 
 
 class PacienteCreateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user', None)
+        self.user = kwargs.pop("user", None)
         super().__init__(*args, **kwargs)
 
         if self.user and self.user.is_authenticated:
-            self.fields['responsavel'].initial = self.user
-            self.fields['responsavel'].disabled = True
+            self.fields["responsavel"].initial = self.user
+            self.fields["responsavel"].disabled = True
 
     def clean(self):
         cleaned_data = super().clean()
@@ -43,7 +45,8 @@ class PacienteCreateForm(forms.ModelForm):
 
         if data_de_nascimento and data_de_nascimento > current_date:
             self.add_error(
-                "data_de_nascimento", "A data de nascimento não pode estar no futuro.")
+                "data_de_nascimento", "A data de nascimento não pode estar no futuro."
+            )
 
         # Validate data_de_internacao (date of admission)
         if data_de_internacao and data_de_internacao > current_date:
@@ -55,32 +58,37 @@ class PacienteCreateForm(forms.ModelForm):
         if data_de_internacao and hora_de_internacao:
             if data_de_internacao == current_date and hora_de_internacao > current_time:
                 self.add_error(
-                    "hora_de_internacao", "A hora de internação não pode estar no futuro."
+                    "hora_de_internacao",
+                    "A hora de internação não pode estar no futuro.",
                 )
 
         # Validation for precisa_de_o2 and tipo_de_o2
         if precisa_de_o2 and not tipo_de_o2:
             self.add_error(
-                "tipo_de_o2", "O tipo de O2 é obrigatório se o paciente precisar de O2.")
+                "tipo_de_o2", "O tipo de O2 é obrigatório se o paciente precisar de O2."
+            )
         elif not precisa_de_o2 and tipo_de_o2:
             self.cleaned_data["tipo_de_o2"] = None
 
         # Validation for precisa_de_vacuo and tipo_de_vacuo
         if precisa_de_vacuo and not tipo_de_vacuo:
             self.add_error(
-                "tipo_de_vacuo", "O tipo de vácuo é obrigatório se o paciente precisar de vácuo.")
+                "tipo_de_vacuo",
+                "O tipo de vácuo é obrigatório se o paciente precisar de vácuo.",
+            )
         elif not precisa_de_vacuo and tipo_de_vacuo:
             self.cleaned_data["tipo_de_vacuo"] = None
 
         # Validation for precisa_de_cirurgia and tipo_de_cirurgia
         if precisa_de_cirurgia and not tipo_de_cirurgia:
             self.add_error(
-                "tipo_de_cirurgia", "O tipo de cirurgia é obrigatório se o paciente precisar de cirurgia.")
+                "tipo_de_cirurgia",
+                "O tipo de cirurgia é obrigatório se o paciente precisar de cirurgia.",
+            )
         elif not precisa_de_cirurgia and tipo_de_cirurgia:
             self.cleaned_data["tipo_de_cirurgia"] = None
 
-        ocupantes = Paciente.objects.filter(
-            leito=leito, removido_em__isnull=True)
+        ocupantes = Paciente.objects.filter(leito=leito, removido_em__isnull=True)
 
         # If editing an existing Paciente, exclude it from the query
         if self.instance and self.instance.pk:
@@ -181,7 +189,7 @@ class PacienteCreateForm(forms.ModelForm):
     )
     leito = forms.ModelChoiceField(
         label="Leito",
-        queryset=Leito.objects.filter(removido_em__isnull=True),
+        queryset=Leito.objects.filter(paciente__isnull=True, removido_em__isnull=True),
         widget=forms.Select(
             attrs={"class": "form-select"},
         ),
@@ -302,30 +310,372 @@ class PacienteCreateForm(forms.ModelForm):
     class Meta:
         model = Paciente
         fields = [
-            'nome',
-            'nome_social',
-            'data_de_nascimento',
-            'data_de_internacao',
-            'hora_de_internacao',
-            'sexo',
-            'diagnostico',
-            'justificativas_pendencias',
-            'problemas_durante_transferencias',
-            'genero',
-            'leito',
-            'responsavel',
-            'unidade_de_saude_de_origem',
-            'medida_de_precaucao',
-            'porta_de_entrada',
-            'especialidade',
-            'status_de_paciente',
-            'nacionalidade_etnia',
-            'gestante',
-            'veio_de_ambulancia',
-            'precisa_de_cirurgia',
-            'tipo_de_cirurgia',
-            'precisa_de_o2',
-            'tipo_de_o2',
-            'precisa_de_vacuo',
-            'tipo_de_vacuo'
+            "nome",
+            "nome_social",
+            "data_de_nascimento",
+            "data_de_internacao",
+            "hora_de_internacao",
+            "sexo",
+            "diagnostico",
+            "justificativas_pendencias",
+            "problemas_durante_transferencias",
+            "genero",
+            "leito",
+            "responsavel",
+            "unidade_de_saude_de_origem",
+            "medida_de_precaucao",
+            "porta_de_entrada",
+            "especialidade",
+            "status_de_paciente",
+            "nacionalidade_etnia",
+            "gestante",
+            "veio_de_ambulancia",
+            "precisa_de_cirurgia",
+            "tipo_de_cirurgia",
+            "precisa_de_o2",
+            "tipo_de_o2",
+            "precisa_de_vacuo",
+            "tipo_de_vacuo",
+        ]
+
+
+class PacienteEditForm(forms.ModelForm):
+    def __init__(self, *args, paciente=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if paciente:
+            self.fields["leito"].queryset = Leito.objects.filter(
+                Q(paciente__isnull=True) | Q(paciente=paciente),
+                removido_em__isnull=True,
+            )
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        leito = cleaned_data.get("leito")
+        precisa_de_o2 = cleaned_data.get("precisa_de_o2")
+        tipo_de_o2 = cleaned_data.get("tipo_de_o2")
+        precisa_de_vacuo = cleaned_data.get("precisa_de_vacuo")
+        tipo_de_vacuo = cleaned_data.get("tipo_de_vacuo")
+        precisa_de_cirurgia = cleaned_data.get("precisa_de_cirurgia")
+        tipo_de_cirurgia = cleaned_data.get("tipo_de_cirurgia")
+        data_de_nascimento = cleaned_data.get("data_de_nascimento")
+        data_de_internacao = cleaned_data.get("data_de_internacao")
+        hora_de_internacao = cleaned_data.get("hora_de_internacao")
+
+        current_date = now().date()
+        current_time = now().time()
+
+        if data_de_nascimento and data_de_nascimento > current_date:
+            self.add_error(
+                "data_de_nascimento", "A data de nascimento não pode estar no futuro."
+            )
+
+        # Validate data_de_internacao (date of admission)
+        if data_de_internacao and data_de_internacao > current_date:
+            self.add_error(
+                "data_de_internacao", "A data de internação não pode estar no futuro."
+            )
+
+        # Validate hora_de_internacao (time of admission)
+        if data_de_internacao and hora_de_internacao:
+            if data_de_internacao == current_date and hora_de_internacao > current_time:
+                self.add_error(
+                    "hora_de_internacao",
+                    "A hora de internação não pode estar no futuro.",
+                )
+
+        # Validation for precisa_de_o2 and tipo_de_o2
+        if precisa_de_o2 and not tipo_de_o2:
+            self.add_error(
+                "tipo_de_o2", "O tipo de O2 é obrigatório se o paciente precisar de O2."
+            )
+        elif not precisa_de_o2 and tipo_de_o2:
+            self.cleaned_data["tipo_de_o2"] = None
+
+        # Validation for precisa_de_vacuo and tipo_de_vacuo
+        if precisa_de_vacuo and not tipo_de_vacuo:
+            self.add_error(
+                "tipo_de_vacuo",
+                "O tipo de vácuo é obrigatório se o paciente precisar de vácuo.",
+            )
+        elif not precisa_de_vacuo and tipo_de_vacuo:
+            self.cleaned_data["tipo_de_vacuo"] = None
+
+        # Validation for precisa_de_cirurgia and tipo_de_cirurgia
+        if precisa_de_cirurgia and not tipo_de_cirurgia:
+            self.add_error(
+                "tipo_de_cirurgia",
+                "O tipo de cirurgia é obrigatório se o paciente precisar de cirurgia.",
+            )
+        elif not precisa_de_cirurgia and tipo_de_cirurgia:
+            self.cleaned_data["tipo_de_cirurgia"] = None
+
+        ocupantes = Paciente.objects.filter(leito=leito, removido_em__isnull=True)
+
+        # If editing an existing Paciente, exclude it from the query
+        if self.instance and self.instance.pk:
+            ocupantes = ocupantes.exclude(pk=self.instance.pk)
+
+        # Check if any Paciente is occupying the leito
+        for paciente in ocupantes:
+            if not Saida.objects.filter(paciente=paciente).exists():
+                self.add_error(
+                    "leito", "Este leito já está sendo ocupado por outro paciente."
+                )
+                break
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        instance.data_de_internacao_no_setor = instance.data_de_internacao
+        instance.hora_de_internacao_no_setor = instance.hora_de_internacao
+
+        if commit:
+            instance.save()
+
+        return instance
+
+    nome = forms.CharField(
+        label="Nome",
+        widget=forms.TextInput(
+            attrs={"class": "form-control"},
+        ),
+    )
+    nome_social = forms.CharField(
+        label="Nome Social",
+        widget=forms.TextInput(
+            attrs={"class": "form-control"},
+        ),
+        required=False,  # Optional field
+    )
+    data_de_nascimento = forms.DateField(
+        label="Data de Nascimento",
+        widget=forms.DateInput(
+            attrs={"class": "form-control", "type": "date"},
+            format="%Y-%m-%d",
+        ),
+        required=False,
+    )
+    data_de_internacao = forms.DateField(
+        label="Data de Internação",
+        widget=forms.DateInput(
+            attrs={"class": "form-control", "type": "date"},
+            format="%Y-%m-%d",
+        ),
+    )
+    hora_de_internacao = forms.TimeField(
+        label="Hora de Internação",
+        widget=forms.TimeInput(
+            attrs={"class": "form-control", "type": "time"},
+            format="%H:%M",
+        ),
+    )
+    sexo = forms.ChoiceField(
+        label="Sexo",
+        choices=[("M", "Masculino"), ("F", "Feminino")],
+        widget=forms.Select(
+            attrs={"class": "form-select"},
+        ),
+    )
+    diagnostico = forms.CharField(
+        label="Diagnóstico",
+        widget=forms.Textarea(
+            attrs={"class": "form-control", "rows": 3},
+        ),
+    )
+    responsavel = forms.ModelChoiceField(
+        label="Leito",
+        queryset=Usuario.objects.filter(status_de_usuario__nome="Ativo"),
+        widget=forms.Select(
+            attrs={"class": "form-select"},
+        ),
+        empty_label=None,
+    )
+    justificativas_pendencias = forms.CharField(
+        label="Justificativas/Pendências",
+        widget=forms.Textarea(
+            attrs={"class": "form-control", "rows": 3},
+        ),
+        required=False,  # Optional field
+    )
+    problemas_durante_transferencias = forms.CharField(
+        label="Problemas Durante Transferências",
+        widget=forms.Textarea(
+            attrs={"class": "form-control", "rows": 3},
+        ),
+        required=False,  # Optional field
+    )
+    genero = forms.ModelChoiceField(
+        label="Gênero",
+        queryset=Genero.objects.all(),
+        widget=forms.Select(
+            attrs={"class": "form-select"},
+        ),
+        empty_label=None,
+    )
+    leito = forms.ModelChoiceField(
+        label="Leito",
+        queryset=Leito.objects.none(),
+        widget=forms.Select(
+            attrs={"class": "form-select"},
+        ),
+        empty_label=None,
+    )
+    unidade_de_saude_de_origem = forms.ModelChoiceField(
+        label="Unidade de Saúde de Origem",
+        queryset=UnidadeDeSaude.objects.filter(removido_em__isnull=True),
+        widget=forms.Select(
+            attrs={"class": "form-select"},
+        ),
+        required=False,  # Optional field
+        empty_label="Nenhuma",
+    )
+    medida_de_precaucao = forms.ModelChoiceField(
+        label="Medida de Precaução",
+        queryset=MedidaDePrecaucao.objects.filter(removido_em__isnull=True),
+        widget=forms.Select(
+            attrs={"class": "form-select"},
+        ),
+        empty_label=None,
+    )
+    porta_de_entrada = forms.ModelChoiceField(
+        label="Porta de Entrada",
+        queryset=PortaDeEntrada.objects.filter(removido_em__isnull=True),
+        widget=forms.Select(
+            attrs={"class": "form-select"},
+        ),
+        empty_label=None,
+    )
+    especialidade = forms.ModelChoiceField(
+        label="Especialidade",
+        queryset=Especialidade.objects.filter(removido_em__isnull=True),
+        widget=forms.Select(
+            attrs={"class": "form-select"},
+        ),
+        empty_label=None,
+    )
+    status_de_paciente = forms.ModelChoiceField(
+        label="Status do Paciente",
+        queryset=StatusDePaciente.objects.filter(removido_em__isnull=True),
+        widget=forms.Select(
+            attrs={"class": "form-select"},
+        ),
+        empty_label=None,
+    )
+    nacionalidade_etnia = forms.ModelChoiceField(
+        label="Nacionalidade/Etnia",
+        queryset=NacionalidadeEtnia.objects.filter(removido_em__isnull=True),
+        widget=forms.Select(
+            attrs={"class": "form-select"},
+        ),
+        empty_label=None,
+    )
+    gestante = forms.BooleanField(
+        label="Gestante",
+        widget=forms.CheckboxInput(
+            attrs={
+                "class": "form-check-input",
+            },
+        ),
+        required=False,  # Optional field
+    )
+    veio_de_ambulancia = forms.BooleanField(
+        label="Veio de Ambulância",
+        widget=forms.CheckboxInput(
+            attrs={
+                "class": "form-check-input",
+            },
+        ),
+        required=False,  # Optional field
+    )
+    precisa_de_cirurgia = forms.BooleanField(
+        label="Precisa de Cirurgia",
+        widget=forms.CheckboxInput(
+            attrs={
+                "class": "form-check-input",
+                "onchange": "toggleInput('id_precisa_de_cirurgia', 'id_tipo_de_cirurgia')",
+            },
+        ),
+        required=False,  # Optional field
+    )
+    tipo_de_cirurgia = forms.ModelChoiceField(
+        label="Tipo de Cirurgia",
+        queryset=TipoDeCirurgia.objects.filter(removido_em__isnull=True),
+        widget=forms.Select(
+            attrs={"class": "form-select", "disabled": ""},
+        ),
+        required=False,  # Optional field
+        empty_label=None,
+    )
+    precisa_de_o2 = forms.BooleanField(
+        label="Precisa de O2",
+        widget=forms.CheckboxInput(
+            attrs={
+                "class": "form-check-input",
+                "onchange": "toggleInput('id_precisa_de_o2', 'id_tipo_de_o2')",
+            },
+        ),
+        required=False,  # Optional field
+    )
+    tipo_de_o2 = forms.ModelChoiceField(
+        label="Tipo de O2",
+        queryset=TipoDeO2.objects.filter(removido_em__isnull=True),
+        widget=forms.Select(
+            attrs={"class": "form-select", "disabled": ""},
+        ),
+        required=False,  # Optional field
+        empty_label=None,
+    )
+    precisa_de_vacuo = forms.BooleanField(
+        label="Precisa de Vácuo",
+        widget=forms.CheckboxInput(
+            attrs={
+                "class": "form-check-input",
+                "onchange": "toggleInput('id_precisa_de_vacuo', 'id_tipo_de_vacuo')",
+            },
+        ),
+        required=False,  # Optional field
+    )
+    tipo_de_vacuo = forms.ModelChoiceField(
+        label="Tipo de Vácuo",
+        queryset=TipoDeVacuo.objects.filter(removido_em__isnull=True),
+        widget=forms.Select(
+            attrs={"class": "form-select", "disabled": ""},
+        ),
+        required=False,  # Optional field
+        empty_label=None,
+    )
+
+    class Meta:
+        model = Paciente
+        fields = [
+            "nome",
+            "nome_social",
+            "data_de_nascimento",
+            "data_de_internacao",
+            "hora_de_internacao",
+            "sexo",
+            "diagnostico",
+            "justificativas_pendencias",
+            "problemas_durante_transferencias",
+            "genero",
+            "leito",
+            "responsavel",
+            "unidade_de_saude_de_origem",
+            "medida_de_precaucao",
+            "porta_de_entrada",
+            "especialidade",
+            "status_de_paciente",
+            "nacionalidade_etnia",
+            "gestante",
+            "veio_de_ambulancia",
+            "precisa_de_cirurgia",
+            "tipo_de_cirurgia",
+            "precisa_de_o2",
+            "tipo_de_o2",
+            "precisa_de_vacuo",
+            "tipo_de_vacuo",
         ]
--- hgr-django/apps/pacientes/views.py
+++ hgr-django/apps/pacientes/views.py
@@ -5,37 +5,38 @@ from django.utils.timezone import now
 from django.db import transaction
 from django.db.models import Q
 from .models import Paciente
-from .forms import PacienteCreateForm
+from apps.leitos.models import Leito
+from apps.historico_de_ocupacao_de_leito.models import HistoricoDeOcupacaoDeLeito
+from .forms import PacienteEditForm, PacienteCreateForm
 
 
 @login_required
 def pacientes_view(request):
-    query = request.GET.get('q', '')
+    query = request.GET.get("q", "")
     if query:
-
         objs = Paciente.objects.filter(
-            Q(nome__icontains=query) |
-            Q(leito__setor__nome__icontains=query) |
-            Q(responsavel__first_name__icontains=query) |
-            Q(status_de_paciente__nome__icontains=query),
+            Q(nome__icontains=query)
+            | Q(leito__setor__nome__icontains=query)
+            | Q(responsavel__first_name__icontains=query)
+            | Q(status_de_paciente__nome__icontains=query),
             removido_em__isnull=True,
-        ).order_by('id')
+        ).order_by("id")
     else:
-        objs = Paciente.objects.filter(removido_em__isnull=True).order_by('id')
+        objs = Paciente.objects.filter(removido_em__isnull=True).order_by("id")
 
     paginator = Paginator(objs, 10)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page_objs = paginator.get_page(page_number)
 
     context = {
-        'caminho': "/gestao/pacientes/",
-        'title': 'Gestão de ' + Paciente._meta.verbose_name_plural,
-        'titulo': Paciente._meta.verbose_name_plural,
-        'mensagem_de_cadastro': 'Cadastrar ' + Paciente._meta.verbose_name,
-        'pacientes': page_objs,
+        "caminho": "/gestao/pacientes/",
+        "title": "Gestão de " + Paciente._meta.verbose_name_plural,
+        "titulo": Paciente._meta.verbose_name_plural,
+        "mensagem_de_cadastro": "Cadastrar " + Paciente._meta.verbose_name,
+        "pacientes": page_objs,
     }
 
-    return render(request, 'pacientes/index.html', context)
+    return render(request, "pacientes/index.html", context)
 
 
 @login_required
@@ -43,24 +44,24 @@ def paciente_view(request, id):
     obj = get_object_or_404(Paciente, id=id, removido_em__isnull=True)
 
     context = {
-        'paciente': obj,
-        'caminho': "/gestao/pacientes/",
-        'title': 'Gestão de ' + Paciente._meta.verbose_name_plural,
-        'titulo': Paciente._meta.verbose_name_plural,
-        'mensagem_de_cadastro': 'Cadastrar ' + Paciente._meta.verbose_name,
+        "paciente": obj,
+        "caminho": "/gestao/pacientes/",
+        "title": "Gestão de " + Paciente._meta.verbose_name_plural,
+        "titulo": Paciente._meta.verbose_name_plural,
+        "mensagem_de_cadastro": "Cadastrar " + Paciente._meta.verbose_name,
     }
 
-    return render(request, 'pacientes/paciente.html', context)
+    return render(request, "pacientes/paciente.html", context)
 
 
 @login_required
 def criar_paciente_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PacienteCreateForm(request.POST, user=request.user)
         if form.is_valid():
             with transaction.atomic():
                 paciente = form.save()
-                leito = form.cleaned_data.get('leito')
+                leito = form.cleaned_data.get("leito")
                 leito.paciente = paciente
                 leito.save()
                 return redirect("/gestao/pacientes/")
@@ -68,26 +69,66 @@ def criar_paciente_view(request):
         form = PacienteCreateForm(user=request.user)
 
     context = {
-        'form': form,
-        'caminho': "/gestao/pacientes/",
-        'title': 'Gestão de ' + Paciente._meta.verbose_name_plural,
-        'titulo': Paciente._meta.verbose_name_plural,
-        'mensagem_de_cadastro': 'Cadastrar ' + Paciente._meta.verbose_name,
+        "form": form,
+        "caminho": "/gestao/pacientes/",
+        "title": "Gestão de " + Paciente._meta.verbose_name_plural,
+        "titulo": Paciente._meta.verbose_name_plural,
+        "mensagem_de_cadastro": "Cadastrar " + Paciente._meta.verbose_name,
     }
 
-    return render(request, 'pacientes/criar.html', context)
+    return render(request, "pacientes/criar.html", context)
 
 
 @login_required
-def editar_paciente_view(request):
-    pass
+def editar_paciente_view(request, id):
+    obj = get_object_or_404(Paciente, id=id, removido_em__isnull=True)
+
+    if request.method == "POST":
+        form = PacienteEditForm(request.POST, instance=obj, paciente=obj)
+        if form.is_valid():
+            with transaction.atomic():
+                antigo_leito = obj.leito
+                leito = form.cleaned_data.get("leito")
+
+                antigo_leito = Leito.objects.select_for_update().get(pk=antigo_leito.pk)
+                leito = Leito.objects.select_for_update().get(pk=leito.pk)
+
+                paciente = form.save()
+
+                if antigo_leito != leito:
+                    antigo_leito.paciente = None
+                    antigo_leito.save()
+
+                    HistoricoDeOcupacaoDeLeito.objects.create(leito=antigo_leito)
+
+                    leito.paciente = paciente
+                    leito.save()
+
+                    HistoricoDeOcupacaoDeLeito.objects.create(
+                        leito=leito, paciente=paciente
+                    )
+
+                return redirect(f"/gestao/pacientes/{id}")
+    else:
+        form = PacienteEditForm(instance=obj, paciente=obj)
+
+    context = {
+        "form": form,
+        "paciente": obj,
+        "caminho": "/gestao/pacientes/",
+        "title": "Gestão de " + Paciente._meta.verbose_name_plural,
+        "titulo": Paciente._meta.verbose_name_plural,
+        "mensagem_de_cadastro": "Editar " + Paciente._meta.verbose_name,
+    }
+
+    return render(request, "pacientes/editar.html", context)
 
 
 @login_required
 def excluir_paciente_view(request, id):
     obj = get_object_or_404(Paciente, id=id, removido_em__isnull=True)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         with transaction.atomic():
             obj.removido_em = now()
             obj.save()
